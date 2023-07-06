b96082616a6fcaf4ae16e594ec6c48a657c23aff
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-07-06 02:29:32 +0100

    high score - level 2
    Level 2
    Present the High Score Tables sorted alphabetically by country name

--- src/components/PlayerScore.js
+++ src/components/PlayerScore.js
@@ -4,6 +4,20 @@ import allCountryScores from "../Scores";
 function PlayerScore() {
   const results = allCountryScores;
 
+  const sortCountryNames = () => {
+    results.sort(function (a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+  sortCountryNames();
+
+
   return results.map((result, index) => (
     <div key={index}>
       <table className="table">
